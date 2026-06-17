ed6baa89c871ddb2fa510a356e616589ffa1e32c
Irina-Ravenna <i.ravenna@proton.me>
2026-06-17 12:11:26 +0200

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
+
+export default defineConfig({
+  site: "https://polaris-six-rust.vercel.app",
+  integrations: [mdx(), react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  image: {
+    service: {
+      entrypoint: "astro/assets/services/sharp",
+    },
+  },
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: "github-light",
+        dark: "github-dark",
+      },
+    },
+  },
+});
